00e16731d03eea0bfdd1ea7afcddb870527ac025
Lukas Lazarek <LukasALazarek@gmail.com>
2019-09-21 12:46:01 -0500

    Add info.rkt and readme

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+
+(define collection "plot-util")
+(define build-deps '("racket-doc" "scribble-lib"))
+(define deps '("gregor-lib"
+               "gui-lib"
+               "plot-gui-lib"
+               "plot-lib"
+               "base" "rackunit"))
