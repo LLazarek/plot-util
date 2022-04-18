e44757e28d129d40b96184df7c42e964058cf29c
Lukas Lazarek <LukasALazarek@gmail.com>
2022-04-18 17:50:08 -0500

    main: add labeled-points utility

--- main.rkt
+++ main.rkt
@@ -1,2 +1,17 @@
 #lang racket/base
 
+(require racket/sequence
+         plot)
+
+(provide labeled-points)
+
+;; like `points`, but all of them labeled
+(define (labeled-points the-points
+                        [point->label (λ (p) (sequence-ref p 0))]
+                        #:anchor [anchor 'auto]
+                        #:angle [angle (label-angle)])
+  (for/list ([p (in-list the-points)])
+    (point-label p
+                 (point->label p)
+                 #:anchor anchor
+                 #:angle angle)))
